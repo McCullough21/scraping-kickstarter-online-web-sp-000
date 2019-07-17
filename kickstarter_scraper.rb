e0afc0f1fb46d86346aee302a52206f44f71e43a
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-17 18:02:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,12 +1,10 @@
 require "nokogiri"
-require 'pry'
-
-
+require "pry"
+ 
 def create_project_hash
-  html = File.read('fictures/kickstarter.html')
-
+  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-binding.pry
+  binding.pry
 end
-
+ 
 create_project_hash
